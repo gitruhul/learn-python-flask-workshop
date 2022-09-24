21465567bc81419bfa7bf7eac4ee5e249fbf421c
Ruhulla.Sheik <sk.ruhulla@gmail.com>
2022-09-24 10:01:09 +0530

    Adding script to create venv

---
+++ venv_script/create_venv.bat
@@ -0,0 +1,8 @@
+REM create folder
+mkdir python_workshop
+REM change directoy to new folder
+cd python_workshop
+REM create a virtual environment
+python -m venv workshopenv
+REM activate virtual environment
+workshopenv\scripts\activate
